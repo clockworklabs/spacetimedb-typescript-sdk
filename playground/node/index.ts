dd0cfe2bf68b2d5c0a42dfb385280903caa5e8db
Puru Vijay <awesomepuruvj@gmail.com>
2024-08-14 13:49:56 +0530

    Working on node!!

--- playground/node/index.ts
+++ playground/node/index.ts
@@ -8,16 +8,13 @@ import { User } from "./module_bindings/user";
 SpacetimeDBClient.registerTables(Message, User);
 SpacetimeDBClient.registerReducers(SendMessageReducer, SetNameReducer);
 
-const client = new SpacetimeDBClient(
-  "ws://localhost:3000",
-  "chat-node",
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJoZXhfaWRlbnRpdHkiOiI3YmEwZWNmOThkNmQ1NzVkZTQ4NTBiZWE0ZmE0MGFmYTE5YmNmMjVjNmQzNmFlMWE0M2M2NGUyMGJlNjIxMTU5IiwiaWF0IjoxNzIzMDE0MjUwLCJleHAiOm51bGx9.aZhU1LL3peqhNih_eDQp-VrgcKvuODZt0nBieV1HAPy82mvAFVDJwaBKWY6zBIFrzORFlcLK9mPjIphhfSmoEA"
-);
+const client = new SpacetimeDBClient("ws://localhost:3000", "chat-node");
 
 client.connect();
 client.on("disconnected", () => {
   console.log("disconnected");
 });
+
 client.on("client_error", () => {
   console.log("client_error");
 });
