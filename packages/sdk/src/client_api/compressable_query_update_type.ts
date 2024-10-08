c4fbf323f91e6dc9618dd5e90b7e972ca44fed0c
Puru Vijay <awesomepuruvj@gmail.com>
2024-10-08 22:31:40 +0530

    Autogenerate

--- packages/sdk/src/client_api/compressable_query_update_type.ts
+++ packages/sdk/src/client_api/compressable_query_update_type.ts
@@ -34,7 +34,9 @@ import {
   SumTypeVariant,
   // @ts-ignore
   TableCache,
-} from '../index';
+  // @ts-ignore
+  deepEqual,
+} from '..';
 // @ts-ignore
 import { QueryUpdate as __QueryUpdate } from './query_update_type';
 
